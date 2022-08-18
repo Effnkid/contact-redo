454b1a73e9f94055a3dda13d9ee355dca56ca20d
Effnkid <l.bobbsemple.gmail.com>
2022-08-18 17:28:33 -0400

    gum 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
-import React from "react";
-import Contact from "./components/Contact";
+import React from 'react';
+import Contact from './components/Contact';
 
 function App() {
+	const contacts = [
+		{
+			name: 'Cory',
+			id: 1,
+			adr: 'Something Something Lane',
+		},
+		{
+			name: 'Zach',
+			id: 2,
+			adr: 'Something Something Lane 2222',
+		},
+		{
+			name: 'Sarah',
+			id: 3,
+			adr: 'Something Something Lane 3333',
+		},
+	];
 
-  const contacts = [
-    {
-      name: "Cory",
-      id:1,
-      adr: "Something Something Lane"
-    },
-    {
-      name: "Zach",
-      id: 2,
-      adr: "Something Something Lane 2222"
-    },
-    {
-      name: "Sarah",
-      id:3,
-      adr: "Something Something Lane 3333"
-    }
-  ]
+	const [list, setList] = React.useState(contacts);
+	const [inp, setInp] = React.useState('');
+	const [change, setChange] = React.useState(false);
 
-  const [list, setList]=React.useState(contacts);
+	React.useEffect(() => {
+		console.log('hit');
+	}, [change]);
 
-  return (
-    <div className="App">
-      {list.map(itm=>
-          <Contact key={itm.id} data={itm} />
-      )}
-    </div>
-  );
+	const submit = () => {
+		let newList = list;
+		const newObj = {
+			name: inp,
+			id: list.length + 1,
+			adr: 'somewhere here',
+		};
+
+		newList.push(newObj);
+		setList(newList);
+		setChange(!change);
+		setInp('');
+	};
+
+	const handleChange = (event) => {
+		setInp(event.target.value);
+	};
+
+	return (
+		<div className="App">
+			{list.map((itm) => (
+				<Contact key={itm.id} data={itm} />
+			))}
+			<input type="text" value={inp} onChange={handleChange} />
+			<button onClick={submit}> Submit</button>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,23 @@
-import React from "react";
-function Contact(props){
+import React from 'react';
+function Contact(props) {
+	const [contacted, setContacted] = React.useState(false);
 
-    const [contacted, setContacted]=React.useState(false);
+	const returnClass = () => {
+		return contacted ? 'contacted' : 'not-contacted';
+	};
 
-    const returnClass = ()=>{
-        return contacted ? "contacted":"not-contacted"
-    }
-
-    return(
-        <div>
-            <h1>{props.data.name}</h1>
-            <p>{props.data.adr}</p>
-            <div className={"contact "+returnClass()}>
-                {contacted ? "Has Been Contacted!": "Needs to be contacted!"}
-            </div>
-        </div>
-    )
+	return (
+		<div className="card">
+			<h1>{props.data.name}</h1>
+			<p>{props.data.adr}</p>
+			<div
+				className={'contact ' + returnClass()}
+				onClick={() => setContacted(!contacted)}
+			>
+				{contacted ? 'Has Been Contacted!' : 'Needs to be contacted!'}
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
